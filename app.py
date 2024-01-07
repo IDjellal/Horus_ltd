65be99db0db5476a8eb1e79b5d659086c842de6a
IDjellal <92578525+IDjellal@users.noreply.github.com>
2024-01-07 20:50:41 +0000

    finalize home page adding name of all pages

--- app.py
+++ app.py
@@ -1,10 +1,30 @@
 from flask import Flask, render_template
 
 app=Flask(__name__)
-
+#run fonction generer et  avec paramettre dans la fonction taux monte 
+#ect...pour faire userrequest
+PAGES=[
+    {
+      'id':1,
+      'Nom Page':'Horus Time Trek',
+      'Descriptif':'Test Horus on Time Trek. Witness past predictions.',
+      },
+    {
+      'id':2,
+      'Nom Page':'HorusCraft Forge',
+      'Descriptif':'Customize at HorusCraft Forge. Generate your own portfolio',
+      },
+    {
+      'id':3,
+      'Nom Page':'Horus Odyssey',
+      'Descriptif':'Dive into Horus vision. Read the manifesto, understand philosophy.',
+      }
+  
+    ]
 @app.route("/")
 def hello():
-  return render_template("home.html")
+  return render_template("home.html",
+                        pages=PAGES)
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0", debug=True)
